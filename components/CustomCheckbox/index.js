9f763572ab1892d900375a98fe745f810d413fd4
artem95dev <mysterydev333@outlook.com>
2021-07-18 19:55:09 -0700

    updated checkbox&buyitem page

--- components/CustomCheckbox/index.js
+++ components/CustomCheckbox/index.js
@@ -8,12 +8,12 @@ import {
 const CustomCheckbox = withStyles({
     root: {
         // background: 'linear-gradient(#FDBF25, #B417EB, #0D57FF, #2D9CB4)',
-        color: 'transparent',
+        color: 'rgba(255, 255, 255, 0.5)',
         '&$checked': {
             color: 'white',
         },
     },
     checked: {},
-})((props) => <Flex justifyContent="center" alignItems="center" w="22px" h="22px" borderRadius="4px" bg="linear-gradient(#FDBF25, #B417EB, #0D57FF, #2D9CB4)"><Checkbox color="default" {...props} /></Flex>);
+})((props) => <Flex justifyContent="center" alignItems="center" w="22px" h="22px" borderRadius="4px" bg="transparent" mr="1rem"><Checkbox color="default" {...props} /></Flex>);
 
 export default CustomCheckbox;
--- pages/buyitem/index.js
+++ pages/buyitem/index.js
@@ -5,38 +5,43 @@ import {
     Image,
     FormLabel,
     ListItem,
-    UnorderedList,
+    List,
 } from '@chakra-ui/react';
 
 const BuyItem = () => {
     return (
         <Flex w="100%" h="100%">
-            <Flex flexDirection="column">
+            {/* <Flex flexDirection="column">
                 <Image alt="ico-twitter"></Image>
                 <Image alt="ico-facebook"></Image>
+            </Flex> */}
+            <Flex w="500px" h="600px">
+                <Image alt="asset image" w="300px" h="300px" src="buyitem/img_item.png"></Image>
             </Flex>
-            <Image alt="asset image"></Image>
             <Flex flexDirection="column">
-                <FormLabel>ADMIRAL GLOVES</FormLabel>
-                <FormLabel>Token ID: 5334...4096</FormLabel>
+                <FormLabel fontSize="42px" textColor="#fff" fontWeight="400">ADMIRAL GLOVES</FormLabel>
                 <Flex>
-                    <Image alt="creator avatar"></Image>
+                    <FormLabel fontSize="16px" fontWeight="400" textColor="#1365F1">Token ID: 5334...4096</FormLabel>
+                    <Image alt="copy addr" w="20px" h="20px" src="buyitem/ico_addr_copy.png" />
+                </Flex>
+                <Flex>
+                    <Image alt="creator avatar" w="50px" h="50px" src="buyitem/img_profile.png"></Image>
                     <Flex flexDirection="column">
-                        <FormLabel>BY CREATOR</FormLabel>
-                        <FormLabel>@KamiSawZe</FormLabel>
+                        <FormLabel fontSize="12px" fontWeight="400" textColor="rgba(255, 255, 255, 0.4)">BY CREATOR</FormLabel>
+                        <FormLabel fontSize="16px" fontWeight="400" textColor="#fff">@KamiSawZe</FormLabel>
                     </Flex>
                 </Flex>
                 <Flex>
                     <Flex>
-                        <Image alt="ico copy"></Image>
+                        <Image alt="ico copy" w="15px" h="15px" src="buyitem/ico_copy.png"></Image>
                         <Text>11 Minted</Text>
                     </Flex>
                     <Flex>
-                        <Image alt="ico mark"></Image>
+                        <Image alt="ico mark" w="15px" h="15px" src="buyitem/ico_note.png"></Image>
                         <Text>8 of 10 For sale</Text>
                     </Flex>
                     <Flex>
-                        <Image alt="ico profile"></Image>
+                        <Image alt="ico profile" w="20px" h="15px" src="buyitem/ico_network.png"></Image>
                         <Text>3 Owners</Text>
                     </Flex>
                 </Flex>
@@ -45,7 +50,7 @@ const BuyItem = () => {
                     <Flex flexDirection="column">
                         <Text>TYPE</Text>
                         <Flex>
-                            <Image alt="ico equip type"></Image>
+                            <Image alt="ico equip type" w="15px" h="15px" src="buyitem/ico_item_type.png"></Image>
                             <Text>Equipment</Text>
                         </Flex>
                     </Flex>
@@ -62,7 +67,7 @@ const BuyItem = () => {
                 </Flex>
                 {/* ------------------------------------Attributes------------------------------------- */}
                 <Flex>
-                    <Image alt="attribute overview img"></Image>
+                    <Image alt="attribute overview img" w="40px" h="40px" src="buyitem/img_attr_type1.png"></Image>
                     <Flex flexDirection="column">
                         <Text>Catalyst</Text>
                         <Text>Legendary</Text>
@@ -70,14 +75,14 @@ const BuyItem = () => {
                     <Flex flexDirection="column">
                         <Text>POWER</Text>
                         <Flex>
-                            <Image alt="power img"></Image>
+                            <Image alt="power img" w="20px" h="20px" src="buyitem/img_def.png"></Image>
                             <Text>49</Text>
                         </Flex>
                     </Flex>
                     <Flex flexDirection="column">
                         <Text>DEFENSE</Text>
                         <Flex>
-                            <Image alt="defense img"></Image>
+                            <Image alt="defense img" w="20px" h="20px" src="buyitem/img_power.png"></Image>
                             <Text>31</Text>
                         </Flex>
                     </Flex>
@@ -96,43 +101,43 @@ const BuyItem = () => {
                     </Flex>
                     {/* --------Gloves------- */}
                     <Flex>
-                        <Image alt="asset img"></Image>
+                        <Image alt="asset img" w="60px" h="60px" src="buyitem/img_item1.png"></Image>
                         <Flex flexDirection="column">
                             <Text>Admiral Gloves</Text>
                             <Text>Legendary</Text>
                             <Text>11</Text>
                         </Flex>
-                        <Image alt="overview img"></Image>
+                        <Image alt="overview img" w="40px" h="40px" src="buyitem/img_attr_type1.png"></Image>
                     </Flex>
                     {/* ---------Jacket--------- */}
                     <Flex>
-                        <Image alt="asset img"></Image>
+                        <Image alt="asset img"  w="60px" h="60px" src="buyitem/img_item1.png"></Image>
                         <Flex flexDirection="column">
                             <Text>Admiral Jacket</Text>
                             <Text>Legendary</Text>
                             <Text>11</Text>
                         </Flex>
-                        <Image alt="overview img"></Image>
+                        <Image alt="overview img" w="40px" h="40px" src="buyitem/img_attr_type2.png"></Image>
                     </Flex>
                     {/* --------------Boots-------------- */}
                     <Flex>
-                        <Image alt="asset img"></Image>
+                        <Image alt="asset img" w="60px" h="60px" src="buyitem/img_item1.png"></Image>
                         <Flex flexDirection="column">
                             <Text>Admiral Boots</Text>
                             <Text>Legendary</Text>
                             <Text>11</Text>
                         </Flex>
-                        <Image alt="overview img"></Image>
+                        <Image alt="overview img" w="40px" h="40px" src="buyitem/img_attr_type2.png"></Image>
                     </Flex>
                 </Flex>
                 {/* -----------------------------Description----------------------------- */}
                 <Flex flexDirection="column">
                     <Text>Description</Text>
-                    <UnorderedList>
-                        <ListItem>When a creator designs an Asset, he has to distribute points amongst the chosen Attributes.</ListItem>
-                        <ListItem>A creator can change the amount of the attributes while creating an asset.</ListItem>
-                        <ListItem>When a Game Maker update is released, new mechanics can use existing attributes or new ones released with the update.</ListItem>
-                    </UnorderedList>
+                    <Flex flexDirection="column">
+                        <Flex><Image alt="list icon" src="buyitem/ico_li.png" w="10px" h="10px" mr="1rem"/>When a creator designs an Asset, he has to distribute points amongst the chosen Attributes.</Flex>
+                        <Flex><Image alt="list icon" src="buyitem/ico_li.png" w="10px" h="10px" mr="1rem"/>A creator can change the amount of the attributes while creating an asset.</Flex>
+                        <Flex><Image alt="list icon" src="buyitem/ico_li.png" w="10px" h="10px" mr="1rem"/>When a Game Maker update is released, new mechanics can use existing attributes or new ones released with the update.</Flex>
+                    </Flex>
                 </Flex>
                 {/* ------------------------------buy component---------------------------- */}
                 <Flex>
