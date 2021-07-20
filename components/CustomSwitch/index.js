57a03cb820fe756aa94af18e0cb5bb10e5029f5c
artem95dev <mysterydev333@outlook.com>
2021-07-19 23:21:23 -0700

    fixed select

--- components/CustomSwitch/index.js
+++ components/CustomSwitch/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Switch from '@material-ui/core/Switch';
+import {
+  Flex,
+} from '@chakra-ui/react';
 
 const CustomSwitch = withStyles((theme) => ({
     root: {
         width: 55,
         height: 26,
         padding: 1,
-        margin: theme.spacing(1),
+        margin: theme.spacing(0),
       },
       switchBase: {
         padding: 1,
@@ -26,8 +29,9 @@ const CustomSwitch = withStyles((theme) => ({
         },
       },
       thumb: {
-        width: 24,
-        height: 24,
+        width: 26,
+        height: 26,
+        marginTop: '-1px',
       },
       track: {
         borderRadius: 26 / 2,
@@ -38,19 +42,22 @@ const CustomSwitch = withStyles((theme) => ({
       checked: {},
       focusVisible: {},
 }))(({ classes, ...props }) => {
+  console.log("class ===>", props);
     return (
-      <Switch
-          focusVisibleClassName={classes.focusVisible}
-          disableRipple
-          classes={{
-          root: classes.root,
-          switchBase: classes.switchBase,
-          thumb: classes.thumb,
-          track: classes.track,
-          checked: classes.checked,
-          }}
-          {...props}
-      />
+      <Flex w="56px" h="28px" borderRadius="14px" bg="transparent" _hover={{ background: classes.checked?'linear-gradient(to left, #FDBF25, #B417EB, #0D57FF, #2D9CB4)': 'transparent' }} padding="2px 2px 2px 0" justifyContent="flex-start" alignItems="center">
+        <Switch
+            focusVisibleClassName={classes.focusVisible}
+            disableRipple
+            classes={{
+            root: classes.root,
+            switchBase: classes.switchBase,
+            thumb: classes.thumb,
+            track: classes.track,
+            checked: classes.checked,
+            }}
+            {...props}
+        />
+      </Flex>
     );
 });
 
