b243e3f2f0ab204d11efc4b43d0466c772953681
starldev <contact@starltoken.com>
2021-07-29 02:22:49 +0900

    approve & placing bid

--- components/BuyModal/index.js
+++ components/BuyModal/index.js
@@ -7,7 +7,8 @@ import {
     ModalOverlay,
     ModalContent,
     IconButton,
-    Input,
+    NumberInput,
+    NumberInputField
 } from "@chakra-ui/react";
 import {
     CloseIcon,
@@ -17,23 +18,80 @@ import {
     placeBid
 } from '../../contracts/auction';
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useWallet } from 'use-wallet';
 import { ethers } from "ethers";
 import BigNumber from "bignumber.js";
-import { approveToken, checkAllowance } from "../../contracts/starlink";
+import {
+    getBalance,
+    approveToken,
+    isTokenApproved
+} from "../../contracts/starlink";
+import {
+    STARL_ADDRESS,
+    SATE_AUCTION_ADDRESS,
+} from "../../utils/const";
+import {
+    getWalletAddress
+} from "../../lib/wallet";
+import {
+    getBigNumber
+} from "../../lib/helper";
 
 const BuyModal = (props) => {
-
     const wallet = useWallet();
+    
+    const [networkId, setNetworkId] = useState(0);
+    const [approved, setApproved] = useState(false);
+    const [pending, setPending] = useState(false);
+    const [bidAmount, setBidAmount] = useState(0);
+
+    const loadData = async () => {
+        if (wallet && wallet.ethereum) {
+            const provider = new ethers.providers.Web3Provider(wallet.ethereum);
+            const network = await provider.getNetwork();
+            setNetworkId(network.chainId);
+            const walletAddr = await getWalletAddress(wallet);
+            let balance = await getBalance(STARL_ADDRESS[network.chainId], walletAddr, provider);
+            const approved = await isTokenApproved(STARL_ADDRESS[network.chainId], walletAddr, SATE_AUCTION_ADDRESS[network.chainId], balance, provider);
+            setApproved(approved);
+        }
+    }
+
+    useEffect(() => {
+        if (wallet && wallet.ethereum) {
+            loadData();
+        }
+    }, [wallet]);
 
     const clickPlaceBid = async () => {
+        if (pending) return;
 
         const provider = new ethers.providers.Web3Provider(wallet.ethereum);
         const signer = await provider.getSigner();
+
+        try {
+            if (approved) {
+                if (!bidAmount || !parseFloat(bidAmount)) return;
+                setPending(true);
+                await placeBid(1, getBigNumber(bidAmount), signer);
+                setPending(false);
+            } else {
+                setPending(true);
+                await approveToken(STARL_ADDRESS[networkId], SATE_AUCTION_ADDRESS[networkId], "115792089237316195423570985008687907853269984665640564039457584007913129639935", signer);
+                setPending(false);
+                setApproved(true);
+            }
+        } catch (e) {
+            console.error(e);
+            setPending(false);
+        }
         
-        await placeBid(1, 0, signer);
     };
+
+    const onChangeAmount = (value) => {
+        setBidAmount(value);
+    }
 
     return (
         <Modal size="lg" isOpen={props.isOpen} onClose={props.onClose}>
@@ -53,24 +111,32 @@ const BuyModal = (props) => {
                     _hover={{}}
                 />
                 <Box mt="1.5rem" bg="#0e1429" borderTopRadius="18px">
-                    <Text color="#fff" fontSize="20px" fontWeight="500" textAlign="center">Place a bid</Text>
+                    <Text color="#fff" fontSize="20px" fontWeight="500" textAlign="center">
+                        Place a bid
+                    </Text>
                 </Box>
                 <Box
                     p="2rem"
                     bg="#0e1429"
                     borderBottomRadius="10px"
                 >
-                    <Text textColor="#fff" fontSize="17px">Price</Text>
+                    <Text textColor="#fff" fontSize="17px">Bid Amount</Text>
                     <Flex alignItems="center" alignSelf="center" borderRadius="5px" h="100%" w="100%" bg="#171e3a" p="0.5rem" mt="1rem">
                         <Image alt="ico search" src="/item/coin_logo.png" w="20px" h="20px" m="0 0.5rem 0 0.5rem" />STARL
-                        <Input textColor="white" ml="1rem" w="100%" _focus={{ outline: 'none'}} placeholder="0.0000" _placeholder={{ color: '#fff' }} bg="#171e3a" border="none" fontSize="15px"/>
+                        <NumberInput
+                            textColor="white" ml="1rem" w="100%" _focus={{ outline: 'none'}} bg="#171e3a" border="none"
+                            value={bidAmount}
+                            onChange={onChangeAmount}
+                        >
+                            <NumberInputField _focus={{ outline: 'none'}} fontSize={["9px", "12px", "12px", "15px"]} border="none" placeholder="0.000"></NumberInputField>
+                        </NumberInput>
                     </Flex>
 
                     <Flex w="100%" justifyContent="center">
                         <Flex as="button" onClick={clickPlaceBid} mt="3rem"  bg="linear-gradient(225deg, #FDBF25, #B417EB, #0D57FF, #2D9CB4)" borderRadius="15px"
                                 _hover={{ background: '#314DFF' }} border="none" _disabled={{ background: '#131A32', textColor: "rgba(255, 255, 255, 0.2)" }} 
                                 textColor="#fff" fontSize="13px" fontWeight="700"
-                                w="50%" h="50px" alignItems="center" justifyContent="center">Place Bid</Flex>
+                                w="50%" h="50px" alignItems="center" justifyContent="center">{approved ? "Place Bid" : "Approve"}</Flex>
                     </Flex>
                 </Box>
             </ModalContent>
--- contracts/starlink.js
+++ contracts/starlink.js
@@ -1,26 +1,53 @@
 import { ethers } from "ethers";
 import abi from './starlink.abi.json';
+import BigNumber from "bignumber.js";
+import { isTransactionMined } from "../lib/helper";
 
-const starlink_address = "0x5a168798df2b9d84e28958702156b036927a9e29";
-
-export async function checkAllowance(owner, sender, signer) {
+export async function getBalance(coinAddress, address, signer) {
     try {
-        const contract = new ethers.Contract(starlink_address, abi, signer);
-        const res = await contract.allowance(owner, sender);
-        console.log({res});
-        return res;
+        const erc20 = new ethers.Contract(coinAddress, abi, signer);
+        const balance = await erc20.balanceOf(address);
+        return balance.toString();    
     } catch (e) {
-        return {e};
+        return "0";
     }
 }
-
-export async function approveToken(sender, amount, signer) {
+  
+export async function getTotalSupply(coinAddress, signer) {
     try {
-        const contract = new ethers.Contract(starlink_address, abi, signer);
-        const res = await contract.approve(sender, amount);
-        console.log({res});
-        return res;
+        const erc20 = new ethers.Contract(coinAddress, abi, signer);
+        const total = await erc20.totalSupply();
+        return total.toString();
+  
     } catch (e) {
-        return {e};
+        return "0";
     }
+}
+  
+export async function isTokenApproved(coinAddress, owner, contract, amount, signer) {
+    try {
+        const erc20 = new ethers.Contract(coinAddress, abi, signer);
+        const allowance = (await erc20.allowance(owner, contract)) || 0;
+        const x = new BigNumber(allowance);
+        const y = new BigNumber(amount);
+        console.log(x);
+        return x.isGreaterThanOrEqualTo(y.isEqualTo("0") ? "10000000":y);
+    } catch (e) {
+        return 0;
+    }
+}
+  
+export async function approveToken(coinAddress, contract, amount, signer) {
+    const erc20 = new ethers.Contract(coinAddress, abi, signer);
+    const { hash } = await erc20.approve(contract, amount);
+    try {
+        while (true) {
+          let mined = await isTransactionMined(hash);
+          if (mined) break;
+        }
+    } catch (e) {
+        console.error(e);
+        return "";
+    }
+    return hash;
 }
